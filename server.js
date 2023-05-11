a948a009484552580d48575db653b01db03aeffa
Dewa Aditya <dewadityasanjaya@gmail.com>
2023-05-11 15:30:42 +0700

    add signin and register endpoint

--- server.js
+++ server.js
@@ -2,10 +2,54 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json())
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'Aditya',
+            email: 'aditya@email.com',
+            password: 'adit1234',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Krisna',
+            email: 'krisna@email.com',
+            password: '4321krisna',
+            entries: 0,
+            joined: new Date()
+        },
+    ]
+}
+
 app.get('/', (req, res) => {
     res.send('Get is working!');
+});
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+        res.json('success');
+    } else {
+        res.status(400).json('error logging in');
+    }
+});
+
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    });
+    res.json(database.users);
 })
 
 app.listen(3000, () => {
-    console.log("Port is working!");
+    console.log("Port 3000 is working!");
 });
