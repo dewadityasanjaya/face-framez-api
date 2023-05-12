157ae28375ecc0c92b1226392e8adb42f134bb4f
Dewa Aditya <dewadityasanjaya@gmail.com>
2023-05-13 01:24:01 +0700

    add bcrypt to store hash of password and update register and profile endpoint

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 // Database Initialization
 const db = knex({
@@ -19,27 +21,6 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'Aditya',
-            email: 'aditya@email.com',
-            password: 'adit1234',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Krisna',
-            email: 'krisna@email.com',
-            password: '4321krisna',
-            entries: 0,
-            joined: new Date()
-        },
-    ]
-}
-
 app.get('/', (req, res) => {
     res.send(database.users);
 });
@@ -54,31 +35,44 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
-    db('users')
-        .returning('*')
-        .insert({
-            name: name,
-            email: email,
-            joined: new Date()
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
         })
-        .then(response => {
-            res.json(response[0]);
-        })
-        .catch(err => res.status(400).json(err))
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0].email,
+                        name: name,
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+    })
+        .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        }
-    })
-    if (!found) {
-        res.status(404).json('User not Found!');
-    }
+    db.select('*').from('users').where({ id })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0])
+            } else {
+                res.status(400).json('Not found')
+            }
+        })
+        .catch(err => res.status(400).json('error getting user'))
 })
 
 app.put('/image', (req, res) => {
